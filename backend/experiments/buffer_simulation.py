0dc950d318b128fc3209347700f2eef2a449fd34
Deveshree Ghanshyam Bhakkad <bhakkaddeveshree@gmail.com>
2026-02-05 22:46:07 +0530

    experiements

--- backend/experiments/buffer_simulation.py
+++ backend/experiments/buffer_simulation.py
@@ -16,22 +16,4 @@ buffer.append(3) #forward
 
 print("Initial buffer:",list(buffer))
 
-def move_to_next_chunk():
-    global current_chunk
 
-    current_chunk +=1
-    buffer.append(current_chunk + FORWARD_LIMIT)
-
-    #enforce backward buffer limit
-
-    while len(buffer) > (BACKWARD_LIMIT + 1 + FORWARD_LIMIT):
-        removed = buffer.popleft()
-        print(f"Removed old chunk: C{removed}")
-
-    print(f"Now playing : C{current_chunk}")
-    print("Buffer state:",list(buffer))
-    print("-" * 40)
-
-for _ in range(5):
-    move_to_next_chunk()
-        
