0abefc684f1eea476da806d1bd36a26e08ca7bf0
Deveshree Ghanshyam Bhakkad <bhakkaddeveshree@gmail.com>
2026-02-12 11:07:33 +0530

    update

--- backend/ml/generate_network_data.py
+++ backend/ml/generate_network_data.py
@@ -13,7 +13,22 @@ def generate_sample():
     jitter = random.uniform(1, 50)               # ms
     packet_loss = random.uniform(0, 5)           # %
 
-    #
+    # Rule-based buffer decision (ground truth)
+    if bandwidth < 800 or latency > 200 or packet_loss > 3:
+        forward_buffer = 5
+    elif bandwidth < 1500 or latency > 120:
+        forward_buffer = 3
+    else:
+        forward_buffer = 2
+
+    return [
+        round(bandwidth, 2),
+        round(latency, 2),
+        round(jitter, 2),
+        round(packet_loss, 2),
+        forward_buffer
+    ]
+
 def generate_dataset(num_samples=1000):
     header = [
         "bandwidth_kbps",
